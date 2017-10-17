cd50484dca78e85a939e11cca1eda11aaa4c48da
Arif Acar <arif@mobven.com>
2017-10-17 14:51:17 +0300

    add quote service

---
+++ spring-common/src/main/java/com/arifacar/common/service/quote/QuoteService.java
@@ -0,0 +1,31 @@
+package com.arifacar.common.service.user;
+
+import com.arifacar.common.exception.EntityNotFoundException;
+import com.arifacar.domain.model.user.User;
+import com.arifacar.domain.repository.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findUserById(Long id) {
+        User user = userRepository.findOne(id);
+        if (user == null)
+            throw new EntityNotFoundException(User.class, Math.toIntExact(id));
+
+        return user;
+    }
+
+    public List<User> findAll() {
+        List<User> users = userRepository.findAll();
+        users.sort(Comparator.comparing(User::getUsername));
+        return users;
+    }
+}
---
+++ spring-common/src/main/java/com/arifacar/common/service/rxJava/FirstRxJava.java
@@ -0,0 +1,7 @@
+package com.arifacar.common.service.rxJava;
+
+/**
+ * Created by arifacar on 03/10/2017.
+ */
+public class FirstRxJava {
+}
